e8ffb2a6a9c696bf5b146453763e0d0eac99ee52
Danilka123456 <73782435+Danilka123456@users.noreply.github.com>
2022-08-12 15:07:25 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFunction() {
-	var x = document.getElementById("myTopnav");
-
-	if(x.className === "topnav") {
-		x.className += " responsive";
-	} else{
-		x.className = "topnav";
-	}
-}
